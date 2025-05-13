105f70690620570976240793dea0d9e0df73391f
beberich <svdadm165@gmail.com>
2025-05-13 23:58:19 +0300

    Add main program logic

--- data_generation.py
+++ data_generation.py
@@ -1,51 +1,78 @@
-import pyspark
-from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from pyspark.sql.types import IntegerType, DateType
 import random
-import datetime
+from datetime import datetime, timedelta, date
 import findspark
 
 findspark.init()
 
 
-def data_quality():
-    """Функция для проверки качества данных"""
-    ...
+def data_quality(df_for_check):
+    """Check data quality"""
+    all_records_cnt = df_for_check.count()
+    records_cnt_after_filters = df_for_check.filter(
+        (length(col('name')) >= 5) & (length(col('city')) >= 7) & (col('age') > 17) & (col('age') < 96) & (
+            col('email').contains('@')) & ((col('email').contains('.ru')) | (col('email').contains('.com')))).count()
+    if all_records_cnt == records_cnt_after_filters:
+        print('Congratulations on data quality is success!')
+    else:
+        print('The data contains an error. Some records have been deleted!')
+
+    return df_for_check
 
 
-def generation_data(*args):
-    """Создание кортежа данных"""
-    return tuple(*args)
+def generation_data(data_count):
+    """Create list of data"""
+    all_data = list()
+    # Example data
+    names = ["Алексей", "Светлана", "Дмитрий", "Виктор", "Иванушка", "Анюта", "Максим", "Ольга", "Анастасия", "Сергей",
+             "Даниил", "Василиса", "Григорий", "Маркиз", "Виктория", "Алина"]
+    cities = ["Санкт-Петербург", "Новосибирск", "Екатеринбург", "Нижний Новгород", "Челябинск", "Ростов-на-Дону",
+              "Красноярск", "Волгоград", "Пятигорск", "Казанкакак", "Сызрань", "Нижневартовск"]
+    letter_for_email = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo',
+                        'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'i', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+                        'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h',
+                        'ц': 'c', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e',
+                        'ю': 'u', 'я': 'ya'}
+    for i in range(1, data_count + 1):
+        name = names[random.randint(0, len(names) - 1)]
+        email = ''.join(letter_for_email[j.lower()] for j in name) + str(
+            random.randint(1000, 9999 + 1)) + '@' \
+                + random.choice(['google', 'yandex', 'bk', 'mail', 'yahoo']) + random.choice(['.ru', '.com'])
+        city = cities[random.randint(0, len(cities) - 1)]
+        age = random.randint(18, 95)
+        salary = random.randint(25000, 500_000 + 1)
+        registration_date = (datetime.now() - timedelta(days=np.random.randint(1, 366))).strftime('%Y-%m-%d')
+        all_data.append((i, name, email, city, age, salary, registration_date))
+
+    return all_data
 
 
-spark = SparkSession.builder.appName("sparkSQL").getOrCreate()
-records_cnt = int(input('Введите количество записей?:'))
-all_data = []
-names = ["Алексей", "Светлана", "Дмитрий", "Виктор", "Иван", "Анна", "Максим", "Ольга", "Анастасия", "Сергей",
-         "Даниил", "Василиса", "Григорий", "Марк", "Виктория", "Алина"]
-cities = ["Санкт-Петербург", "Новосибирск", "Екатеринбург", "Нижний Новгород", "Челябинск", "Ростов-на-Дону",
-          "Красноярск", "Волгоград", "Самара", "Казань", "Сызрань", "Нижневартовск"]
-letter_for_email = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo',
-                    'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'i', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
-                    'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h',
-                    'ц': 'c', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e',
-                    'ю': 'u', 'я': 'ya'}
+def create_dataset(all_data):
+    """Create dataframe on list of data"""
+    global spark  # временное решение
+    spark = SparkSession.builder.appName("sparkSQL").getOrCreate()
+    column_name = ["id", "name", "email", "city", "age", "salary", "registration_date"]
+    test_df = spark.createDataFrame(all_data, column_name)
+    test_df = test_df \
+        .withColumn('age', col('age').cast(IntegerType())) \
+        .withColumn('registration_date', col('registration_date').cast(DateType()))
+    return test_df
 
-column_name = ["id", "name", "email", "city", "age", "salary", "registration_date"]
 
-for i in range(1, records_cnt + 1):
-    name = names[random.randint(0, len(names) - 1)]
-    email = ''.join(letter_for_email[j.lower()] for j in name) + str(
-        random.randint(1000, 9999)) + '@' \
-            + random.choice(['google', 'yandex', 'bk', 'mail', 'yahoo']) + random.choice(['.ru', '.com', '.bk'])
-    city = cities[random.randint(0, len(cities) - 1)]
-    age = random.randint(18, 95)
-    salary = random.randint(25000, 500_000)
-    registration_date = 'registration_date'
-    all_data.append(generation_data([i, name, email, city, age, salary, registration_date]))
+records_cnt = int(input('Введите количество записей: '))  # пофиксить ввод
 
-df = spark.createDataFrame(all_data, column_name)
-df.show()
+# Data manipulation
+all_records = generation_data(records_cnt)
+
+df = create_dataset(all_records)
+
+df_after_dq = data_quality(df)
+
+today = datetime.date(datetime.now())
+df_pd = df_after_dq.toPandas()
+df_pd.set_index('id', inplace=True)
+df_pd.to_csv(f'{today.year}-{today.month}-{today.day}-dev.csv')
 
 spark.stop()
